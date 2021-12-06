b88cca44bd3f2f3c3e7d0862d3124c1d5e742af0
Rıza Mert YILDIZTAS <rmyildiztas@gmail.com>
2021-12-06 18:24:51 +0300

    Models added

---
+++ ProductApp/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductApp.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Sales> sales { get; set; }
+    }
+}
---
+++ ProductApp/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerLastName { get; set; }
+    }
+}
