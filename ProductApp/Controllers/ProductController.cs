48bb72e5aac5d0e30b8bbb5b6579bb962025dce1
Rıza Mert YILDIZTAS <rmyildiztas@gmail.com>
2021-12-08 13:04:28 +0300

    Category, customer and product listing pages with the simplest model

--- ProductApp/Controllers/ProductController.cs
+++ ProductApp/Controllers/ProductController.cs
@@ -9,8 +9,30 @@ namespace ProductApp.Controllers
         Context c =new Context();
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Category()
+        {
             var cat = c.TB_CATEGORIES.ToList();
             return View(cat);
+        }
+        [HttpGet]
+        public IActionResult Customer()
+        {
+            var cst = c.TB_CUSTOMERS.ToList();
+            return View(cst);
+        }
+        [HttpGet]
+        public IActionResult Product()
+        {
+            var pr = c.TB_PRODUCTS.ToList();
+            return View(pr);
+        }
+        public IActionResult Sales()
+        {
+            return View();
         }
     }
 }
