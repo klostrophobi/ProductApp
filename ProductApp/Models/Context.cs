ea14a73fa90202c05527a7d14af657eff1fc5966
Rıza Mert YILDIZTAS <rmyildiztas@gmail.com>
2021-12-08 12:07:03 +0300

    Migration Trial, DB Connections and Category page added

--- ProductApp/Models/Context.cs
+++ ProductApp/Models/Context.cs
@@ -4,9 +4,15 @@ namespace ProductApp.Models
 {
     public class Context : DbContext
     {
-        public DbSet<Category> categories { get; set; }
-        public DbSet<Customer> customers { get; set; }
-        public DbSet<Product> products { get; set; }
-        public DbSet<Sales> sales { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MVCApp;Integrated Security=True");
+            //optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ProductApp;Integrated Security=True");
+        }
+
+        public DbSet<Category> TB_CATEGORIES { get; set; }
+        public DbSet<Customer> TB_CUSTOMERS { get; set; }
+        public DbSet<Product> TB_PRODUCTS { get; set; }
+        public DbSet<Sales> TB_SALES { get; set; }
     }
 }
